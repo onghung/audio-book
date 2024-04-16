25e4c607be1ec3beb299c98269e20fa6a99224e9
onghung <93970366+onghung@users.noreply.github.com>
2024-04-17 00:35:24 +0700

    commit GD1
    - cấu trúc thư mục
    - kết nối server
    - test api
    - test hiển thị

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:tlubook/views/book_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: "SourceSansPro"),
+      home: MyView(),
+    );
+  }
+}
+
+
